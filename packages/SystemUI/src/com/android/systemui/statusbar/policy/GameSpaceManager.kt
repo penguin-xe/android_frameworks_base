LMODroid/platform_frameworks_base

fwb: Fix gamespace on secondary users
Change-Id: I80c23dbfe900253510d027c891b2e6f441c45132

packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
@@ -126,12 +126,15 @@ class GameSpaceManager @Inject constructor(
     fun isGameActive() = activeGame != null
 
     fun shouldSuppressFullScreenIntent() =
-        Settings.System.getInt(context.contentResolver,
-            LMOSettings.System.GAMESPACE_SUPPRESS_FULLSCREEN_INTENT, 0) == 1 && isGameActive()
+        Settings.System.getIntForUser(context.contentResolver,
+            LMOSettings.System.GAMESPACE_SUPPRESS_FULLSCREEN_INTENT, 0,
+            UserHandle.USER_CURRENT) == 1 && isGameActive()
 
     private fun checkGameList(packageName: String?): String? {
         packageName ?: return null
-        val games = Settings.System.getString(context.contentResolver, LMOSettings.System.GAMESPACE_GAME_LIST)
+        val games = Settings.System.getStringForUser(context.contentResolver,
+                LMOSettings.System.GAMESPACE_GAME_LIST,
+                UserHandle.USER_CURRENT)
         if (games.isNullOrEmpty())
             return null
 

services/core/java/com/android/server/app/GameManagerService.java
@@ -2246,8 +2246,8 @@ public final class GameManagerService extends IGameManagerService.Stub {
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            String newValue = Settings.Secure.getString(mContentResolver,
-                    LMOSettings.Secure.GAME_OVERLAY);
+            String newValue = Settings.Secure.getStringForUser(mContentResolver,
+                    LMOSettings.Secure.GAME_OVERLAY, UserHandle.USER_CURRENT);
             if (newValue == null) return;
             // We write key and value of the device_config property as a single string
             // from our GameSpace.

penguin-xe/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
@@ -124,12 +124,14 @@ class GameSpaceManager @Inject constructor(
     fun isGameActive() = activeGame != null
 
     fun shouldSuppressFullScreenIntent() =
-        Settings.System.getInt(context.contentResolver,
-            Settings.System.GAMESPACE_SUPPRESS_FULLSCREEN_INTENT, 0) == 1 && isGameActive()
+        Settings.System.getIntForUser(context.contentResolver,
+            Settings.System.GAMESPACE_SUPPRESS_FULLSCREEN_INTENT, 0,
+            UserHandle.USER_CURRENT) == 1 && isGameActive()
 
     private fun checkGameList(packageName: String?): String? {
         packageName ?: return null
-        val games = Settings.System.getString(context.contentResolver, Settings.System.GAMESPACE_GAME_LIST)
+        val games = Settings.System.getStringForUser(context.contentResolver,
+                Settings.System.GAMESPACE_GAME_LIST, UserHandle.USER_CURRENT)
         if (games.isNullOrEmpty())
             return null
 

services/core/java/com/android/server/app/GameManagerService.java
@@ -2244,8 +2244,8 @@ public final class GameManagerService extends IGameManagerService.Stub {
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            String newValue = Settings.Secure.getString(mContentResolver,
-                    Settings.Secure.GAME_OVERLAY);
+	    String newValue = Settings.Secure.getStringForUser(mContentResolver,
+                    Settings.Secure.GAME_OVERLAY, UserHandle.USER_CURRENT);
             if (newValue == null) return;
             // We write key and value of the device_config property as a single string
             // from our GameSpace.