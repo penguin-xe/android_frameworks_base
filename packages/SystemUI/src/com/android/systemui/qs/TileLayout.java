4dd69426ef09b9a992a20d2a9bf03a0de522cdb8
rbnj <rbnj2112@gmail.com>
2025-01-02 00:09:48 +0530

    Revert "SystemUI: Add settings to change QS column count [1/2]"
    This reverts commit bb1da74a0465de63040009c8eb7e82d240656aa9.

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -1,7 +1,6 @@
 package com.android.systemui.qs;
 
 import static com.android.systemui.util.Utils.useQsMediaPlayer;
-import static com.android.systemui.util.qs.QSStyleUtils.isRoundQS;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -101,11 +100,6 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         return updateColumns();
     }
 
-    @Override
-    public int getMaxColumns() {
-        return mMaxColumns;
-    }
-
     public void addTile(TileRecord tile) {
         mRecords.add(tile);
         tile.tile.setListening(this, mListening);
@@ -158,10 +152,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
 
     public boolean updateColumns() {
         int oldColumns = mColumns;
-        if (isRoundQS())
-            mColumns = mMaxColumns;
-        else
-            mColumns = Math.min(mResourceColumns, mMaxColumns);
+        mColumns = Math.min(mResourceColumns, mMaxColumns);
         return oldColumns != mColumns;
     }
 
