57ad7060263d617ef5e666e1ed7e2e9b388d5b2c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-18 18:35:25 +0800

    SystemUI: Use notification_corner_radius for media player scrollview corners
    Change-Id: Ife3f02942584915762510113f64455faa624959d
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/MediaCarouselScrollHandler.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/MediaCarouselScrollHandler.kt
@@ -216,7 +216,7 @@ class MediaCarouselScrollHandler(
         // reinflated appropriately
         cornerRadius =
             settingsButton.resources.getDimensionPixelSize(
-                Utils.getThemeAttr(settingsButton.context, android.R.attr.dialogCornerRadius)
+                R.dimen.notification_corner_radius
             )
         updateSettingsPresentation()
         scrollView.invalidateOutline()
